0f7a6ae46caecec493ece780773bad111496e91a
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2024-07-04 06:22:01 +0200

    First Commit
    - font
    -bg color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from './head/head.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HeadComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
