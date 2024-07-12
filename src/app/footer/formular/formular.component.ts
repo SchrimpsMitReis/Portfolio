59b9da58838974bb45c992b81c1fe04e100033bb
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2024-07-12 06:41:31 +0200

    Formular

--- src/app/footer/formular/formular.component.ts
+++ src/app/footer/formular/formular.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formular',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './formular.component.html',
   styleUrl: './formular.component.scss'
 })
 export class FormularComponent {
 
+  contactData = {
+    name: "",
+    email: "",
+    text: "",
+    policy: false
+  }
+
+  onSubmit(){
+    console.log(this.contactData);
+    
+  }
 }
