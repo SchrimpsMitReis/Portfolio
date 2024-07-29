4e1964cf3199a2834519b7ea3546995dd5b0ea78
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2024-07-29 12:07:24 +0200

    29.07.2024

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
+
+  language!: any;
+  text!: string;
+  deutsch: string = `Hi, ich bin Roman Schröder, ein kreativer und logikbegeisterter Profi aus Remagen. 
+  Mit einem Bachelor in Digital Film Making, spezialisiert auf Postproduktion und Inklusion, sowie einer Weiterbildung zum Frontend Developer, 
+  bringe ich eine einzigartige Mischung aus Kreativität und technischem Know-how mit.
+  Ich freue mich darauf, meine Reise fortzusetzen und bin gespannt auf neue Herausforderungen und Möglichkeiten, bei denen ich meine vielfältigen Fähigkeiten einbringen kann.`
+
+  ngOnInit() {
+    this.language = navigator.language;
+    this.setLang()
+    console.log(this.language);
+  }
+
+  setLang(){
+    if (this.language === "de"){
+      this.text = this.deutsch;
+
+    }
+  }
 
 
 
+  // Mögliche sprachen "de", "en-US", "en"
 }
