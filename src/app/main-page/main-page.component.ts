82ee28ca97e970f425deb63f80104f65aced29f6
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2024-08-10 11:11:39 +0200

    final 2

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeadComponent } from '../head/head.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { FooterComponent } from '../footer/footer.component';
 import { MySkillsComponent } from '../my-skills/my-skills.component';
 import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
+    CommonModule,
     HeadComponent,
     AboutMeComponent,
     MySkillsComponent,
@@ -17,6 +19,5 @@ import { PortfolioComponent } from '../portfolio/portfolio.component';
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
-export class MainPageComponent {
-
+export class MainPageComponent{
 }
